44d955820435a79d3938f2f4c4bf6634df594032
Gildoas2 <gildo@bettermaps.ai>
2024-11-08 10:30:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("HELLO")
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # Usa el puerto que Cloud Run espera
+    app.run(host="0.0.0.0", port=port)
